c72c72a218e00b71a9bfbf680ff3d22cfa987706
badr <fadre6@gmail.com>
2026-05-06 00:15:00 +0100

    feat(exceptions): add domain exceptions for customer, account and balance

---
+++ backend/src/main/java/org/sid/ebankingbackend/exceptions/BalanceNotSufisantException.java
@@ -0,0 +1,7 @@
+package org.sid.ebankingbackend.exceptions;
+
+public class BalanceNotSufisantException extends Exception {
+    public BalanceNotSufisantException(String message) {
+        super(message);
+    }
+}
